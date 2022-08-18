598f75cb55bd50bd7326d330b99e1aa39a059965
Micaias <gutierrezmicaias@gmail.com>
2022-08-18 19:05:23 -0300

    rutas ok

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,31 +1,65 @@
 const express = require('express');
-// const { Router } = require('express');
 const axios = require ("axios");
 
 const app= express();
 
-// const router = Router();
-
 app.get("/films", async (req, res) => {
     try {
         const responseApi = await axios("https://swapi.dev/api/films");
-        // console.log(responseApi.data.results);
-        const movies = responseApi.data.results.map((p) => {
+        const movies = await Promise.all(responseApi.data.results.map(async (peli) => {
             return {
-                title: p.title,
-                release_date: p.release_date,
-                director: p.director,
-                producer: p.producer,
-                opening_crawl: p.opening_crawl,  
-                characters: p.characters
+                episode_id: peli.episode_id,
+                title: peli.title,
+                release_date: peli.release_date,
+                director: peli.director,
+                producer: peli.producer,
+                opening_crawl: peli.opening_crawl,
+                characters: await Promise.all(peli.characters.map((c) => axios(c).then(res => res.data)
+                            .then(data => {
+                                return {
+                                    name: data.name,
+                                    gender: data.gender,
+                                    birth_year: data.birth_year,
+                                    height: data.height,
+                                    mass: data.mass,
+                                    id: data.url.slice(29, -1)
+                                }
+                            })))
             }
-        })
-    //    const charac = movies.map(async (c) =>  await c.characters.map(async (p) => await axios(p)))
-        // console.log(charac)
-       return res.status(200).send(movies);
+        }));
+        return res.status(200).send(movies);
         
     } catch (error) {
         console.log(error);        
+    }
+});
+
+app.get("/character/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const character = await axios(`https://swapi.dev/api/people/${id}`).then(res => res.data)
+                            .then(async (info) => {
+                                return {
+                                    name: info.name,
+                                    gender: info.gender,
+                                    birth_year: info.birth_year,
+                                    height: info.height,
+                                    mass: info.mass,
+                                    starships: await Promise.all(info.starships.map((s) => axios(s).then(res => res.data)
+                                                .then(data => {
+                                                    return {
+                                                        name: data.name,
+                                                        model: data.model,
+                                                        starship_class: data.starship_class,
+                                                        passengers: data.passengers
+                                                    }
+                                                })))
+                                }
+                            });
+
+        return res.status(200).send(character);
+    } catch (error) {
+        console.log(error);
     }
 });
 
